4a79e03ab7fd84ee0f1eb79a001ab3ffdac3e1f8
Vik1ang <vik1ang@outlook.com>
2021-11-22 23:55:52 +0800

    go study 02

--- GoBasicStudy/src/study02/main.go
+++ GoBasicStudy/src/study02/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Printf("%T\n", 123.3)
+
+	var string1 string
+	string1 = "123"
+	fmt.Printf("%T\n", string1)
+	int111, _ := strconv.Atoi(string1)
+	fmt.Println(int111)
+	fmt.Printf("%T\n", int111)
+
 }
